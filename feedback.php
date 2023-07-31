2008554f7b7109e1a0113cde2040b7413b3b2f2e
Leo <leocsdev@gmail.com>
2023-07-31 23:02:38 +0800

    L05, fetch data from db.

--- feedback.php
+++ feedback.php
@@ -1,26 +1,10 @@
 <?php include 'inc/header.php'; ?>
 
 <?php
-$feedback = [
-  [
-    'id' => 1,
-    'name' => 'Jene Santi',
-    'email' => 'jene@example.com',
-    'body' => 'I love this site.'
-  ],
-  [
-    'id' => 2,
-    'name' => 'Thea Santi',
-    'email' => 'thea@example.com',
-    'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni, quibusdam.'
-  ],
-  [
-    'id' => 3,
-    'name' => 'Leeah Santi',
-    'email' => 'leeah@example.com',
-    'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
-  ],
-]
+// Fetch feedback data from database
+$sql = 'SELECT * FROM feedback';
+$result = mysqli_query($conn, $sql);
+$feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
 
 <div class="container d-flex flex-column align-items-center">
@@ -34,39 +18,12 @@ $feedback = [
         <div class="card-body text-center">
           <?php echo $item['body']; ?>
           <div class="text-secondary mt-2">
-            By <?php echo $item['name']; ?>
+            By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
           </div>
         </div>
       </div>
     <?php endforeach; ?>
   <?php endif; ?>
-
-  <!-- <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-      molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-      repellendus voluptatibus natus deserunt sed doloribus inventore,
-      totam labore maxime perferendis!
-    </div>
-  </div>
-
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-      molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-      repellendus voluptatibus natus deserunt sed doloribus inventore,
-      totam labore maxime perferendis!
-    </div>
-  </div>
-
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
-      molestias animi earum eos dolorem repellat a quibusdam, aperiam vero
-      repellendus voluptatibus natus deserunt sed doloribus inventore,
-      totam labore maxime perferendis!
-    </div>
-  </div> -->
 </div>
 
 <?php include 'inc/footer.php'; ?>
